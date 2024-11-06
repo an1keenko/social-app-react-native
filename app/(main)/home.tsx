b205d9e87c65089c49ef876c2b4d440a7ed22d41
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2024-11-06 21:12:50 +0100

    feat: layout

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
 import { Text, View } from 'react-native';
+import ScreenWrapper from '@/components/ScreenWrapper';
+import {useAuth} from "@/contexts/AuthContext";
+import {supabase} from "@/lib/supabase";
+import Button from "@/components/Button";
 
 const Home = () => {
+    const {setAuth} = useAuth();
+
+    const onLogout = async () => {
+        setAuth(null);
+        const {error} = await supabase.auth.signOut();
+        if (error) {
+            console.log('Sign out', 'Error signing out!');
+        }
+    }
+
   return (
-    <View>
+    <ScreenWrapper>
       <Text>Home</Text>
-    </View>
+        <Button title="Log out" onPress={onLogout}></Button>
+    </ScreenWrapper>
   );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from 'react';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { AuthProvider, useAuth } from '@/contexts/AuthContext';
 import { supabase } from '@/lib/supabase';
 
 const _layout = () => {
-  // const {setAuth} = useAuth();
-  //
+  const { setAuth } = useAuth();
+  const router = useRouter();
+
   useEffect(() => {
     supabase.auth.onAuthStateChange((_event, session) => {
       console.log('session', session);
+
+      if (session) {
+        setAuth(session?.user);
+        router.push('/home');
+      } else {
+        setAuth(null);
+        router.replace('/welcome');
+      }
     });
   }, []);
 
