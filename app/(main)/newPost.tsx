ffe59f9c53d500b2ac50183c0ef968977f15d7c1
Ihar Anikeyenka <ihar.anikeyenka@n1affiliates.com>
2024-12-03 00:37:32 +0100

    feat: create post

--- app/(main)/newPost.tsx
+++ app/(main)/newPost.tsx
@@ -1,13 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import ScreenWrapper from '@/components/ScreenWrapper';
+import Header from '@/components/Header';
+import { hp, wp } from '@/helpers/common';
+import { useAuth } from '@/contexts/AuthContext';
+import { theme } from '@/constants/theme';
+import Avatar from '@/components/Avatar';
+import RichTextEditor from '@/components/RichTextEditor';
+import { useRef, useState } from 'react';
+import { useRouter } from 'expo-router';
 
 const NewPost = () => {
+  const { user } = useAuth();
+  const bodyRef = useRef('');
+  const editorRef = useRef(null);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState(null);
+
   return (
-    <View>
-      <Text>Notifications</Text>
-    </View>
+    <ScreenWrapper background="white">
+      <View style={styles.container}>
+        <Header title="Create Post" />
+        <ScrollView contentContainerStyle={{ gap: 20 }}>
+          <View style={styles.header}>
+            <Avatar uri={user?.image} size={hp(6.5)} rounded={theme.radius.xl} />
+            <View style={{ gap: 2 }}>
+              <Text style={styles.username}>{user && user.name}</Text>
+              <Text style={styles.publicText}>Public</Text>
+            </View>
+          </View>
+          <View style={styles.textEditor}>
+            <RichTextEditor editorRef={editorRef} onChange={(body) => (bodyRef.current = body)} />
+          </View>
+          <View style={styles.media}></View>
+        </ScrollView>
+      </View>
+    </ScreenWrapper>
   );
 };
 
 export default NewPost;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 30,
+    paddingHorizontal: wp(4),
+    gap: 15,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+  username: {
+    fontSize: hp(2.2),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  publicText: {
+    fontSize: hp(1.8),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.textLight,
+  },
+  textEditor: {
+    // marginTop: 10,
+  },
+  media: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1.5,
+    padding: 12,
+    paddingHorizontal: 18,
+    borderRadius: theme.radius.xl,
+    borderCurve: 'continuous',
+    borderColor: theme.colors.gray,
+  },
+  mediaIcons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+  },
+});
